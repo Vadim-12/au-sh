450577239076eb586576ac0e75973cc5f9d22351
Vadim-12 <66643471+Vadim-12@users.noreply.github.com>
2023-04-01 20:41:17 +0300

    _

--- api/dist/src/cars/cars.controller.d 3.ts
+++
@@ -1,8 +0,0 @@
-import { CarsService } from './cars.service';
-import { CreateCarDto } from './dto/create-car.dto';
-export declare class CarsController {
-    private readonly carsService;
-    constructor(carsService: CarsService);
-    getAllCars(): Promise<import("../../types/car").Car[]>;
-    createCar(carDto: CreateCarDto): Promise<import("../../types/car").Car>;
-}
--- api/dist/src/cars/cars.service.d 3.ts
+++
@@ -1,9 +0,0 @@
-import { Car } from 'types/car';
-import { CarModel } from './car.model';
-import { CreateCarDto } from './dto/create-car.dto';
-export declare class CarsService {
-    private carsRepository;
-    constructor(carsRepository: typeof CarModel);
-    createCar(dto: CreateCarDto): Promise<Car>;
-    getAllCars(): Promise<Car[]>;
-}
