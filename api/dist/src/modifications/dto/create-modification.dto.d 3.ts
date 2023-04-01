df48b55c38469ad63563e2026bda62250705e67e
Vadim-12 <66643471+Vadim-12@users.noreply.github.com>
2023-04-01 22:44:04 +0300

    _

--- api/dist/src/modifications/dto/create-modification.dto.d 3.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateModificationDto {
-    car_ID: number;
-    engine_ID: number;
-    price_min: number;
-    price_max: number;
-    capacity: number;
-    power: number;
-    WD_ID: number;
-}
