e9cc6ae5f527d2124511b7cbfd6f93a439346614
Vadim-12 <66643471+Vadim-12@users.noreply.github.com>
2023-04-03 13:45:54 +0300

    _

--- api/dist/src/modifications/modifications.service.d 3.ts
+++
@@ -1,9 +0,0 @@
-import { ModificationModel } from './modification.model';
-import { CreateModificationDto } from './dto/create-modification.dto';
-import { Modification } from 'types/modification';
-export declare class ModificationsService {
-    private modificationsRepository;
-    constructor(modificationsRepository: typeof ModificationModel);
-    createModification(dto: CreateModificationDto): Promise<Modification>;
-    getModificationsOfCar(car_ID: number): Promise<Modification[]>;
-}
