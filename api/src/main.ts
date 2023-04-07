17c4b6d2b2cade8f5cbeb2fa90bccb1c6185d5b4
Vadim-12 <66643471+Vadim-12@users.noreply.github.com>
2023-04-07 16:43:25 +0300

    init front

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = Number(process.env.PORT) || 5000
 
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {cors: false})
 
   app.enableCors({
     origin: 'https://genise-tk.ru'
